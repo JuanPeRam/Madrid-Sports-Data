e622a81916770f2a9f1ae0a4e3bd998513248b49
Juan Pereira <juan.cpereira@hotmail.com>
2024-03-04 13:19:36 +0100

    Updated Web

--- madrid-competitions-website/src/components/sections/Sports.tsx
+++ madrid-competitions-website/src/components/sections/Sports.tsx
@@ -1,16 +1,55 @@
+import { useState } from "react"
+import { Skeleton } from "../ui/skeleton"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+
 interface SportsProps {
-  sports:string[]
+  sports:string[]|undefined
   onSportChanged: (sport:string)=>void
+  isLoading:boolean
+  sportSelected:string
 }
 
-export const Sports:React.FC<SportsProps> = ({sports, onSportChanged}) => {
+export const Sports:React.FC<SportsProps> = ({sports, onSportChanged,isLoading,sportSelected}) => {
+
+  const [selectedSport, setSelectedSport] = useState(sportSelected ?? '');
+  const shownSports = sports?.slice(0,8)
+  const hiddenSports = sports?.slice(8)
   return (
-    <section className="flex gap-4 p-5">
+    <section className="flex gap-4 p-5 bg-border">
       {
-        sports && 
-        sports.map((sport)=>(
-          <button onClick={()=>onSportChanged(sport)}>{sport}</button>
-        ))
+        sports && !isLoading &&
+        <section className="flex gap-5 text-center w-full justify-center items-center flex-wrap">
+          {
+            shownSports?.map((sport:string)=>(
+              <div className={`cursor-pointer hover:bg-primary/20 flex items-center justify-center p-3 ${selectedSport===sport ? 'bg-primary/40':''}`} onClick={()=>{onSportChanged(sport);setSelectedSport(sport)}} key={sport}>{sport}</div>
+            ))
+          }
+          <Popover>
+            <PopoverTrigger>Ver más</PopoverTrigger>
+            <PopoverContent className="flex flex-wrap justify-center items-center gap-2">
+                {
+                  hiddenSports?.map((sport:string)=>(
+                    <div key={sport} className="cursor-pointer hover:bg-primary/20 flex items-center justify-center p-2">
+                      {sport}
+                    </div>
+                  ))
+                }
+            </PopoverContent>
+          </Popover>
+          
+        </section>
+        
+      }
+      {
+        !sports &&
+        <div className="flex h-full w-full">
+          <Skeleton className="w-10 h-full rounded-sm"></Skeleton>
+          <Skeleton className="w-10 h-full rounded-sm"></Skeleton>
+          <Skeleton className="w-10 h-full rounded-sm"></Skeleton>
+          <Skeleton className="w-10 h-full rounded-sm"></Skeleton>
+          <Skeleton className="w-10 h-full rounded-sm"></Skeleton>
+          <Skeleton className="w-10 h-full rounded-sm"></Skeleton>
+        </div>
       }
     </section>
   )
--- madrid-competitions-website/src/pages/Home.tsx
+++ madrid-competitions-website/src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import { Rankings } from "@/components/Rankings"
+import { SeasonsSelect } from "@/components/SeasonsSelect"
 import { MainContent } from "@/components/sections/MainContent"
 import { NavBar } from "@/components/sections/NavBar"
 import { Sports } from "@/components/sections/Sports"
-import { useState } from "react"
+import { API_ENDPOINT } from "@/constants/global"
+import { RankingItem } from "@/types/Rankings"
+import { useEffect, useState } from "react"
 
 export const Home = () => {
 
     const [season, setSeason] = useState<string>('');
+    const [data, setData] = useState<RankingItem[]>([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [sportsArray, setSportsArray] = useState<string[]|[]>([]);
     const [filters, setFilters] = useState({
         Nombre_deporte: '',
         Nombre_distrito: '',
@@ -20,18 +26,34 @@ export const Home = () => {
           [filterName]: value
         });
       };
-
-    const handleSportChanged = (sport:string)=>{
-        console.log(sport)
+    
+    const handleSeasonChange = (season:string)=>{
+      setSeason(season)
     }
+
+    useEffect(()=>{
+        if(season){
+            setIsLoading(true)
+            const endpoint = `${API_ENDPOINT}/ranking/${season.split('/')[0]}%2F${season.split('/')[1]}`
+            fetch(endpoint)
+                .then((res)=>res.json())
+                .then((data)=>setData(data))
+                .catch((error)=>console.log(error))
+                .finally(()=>setIsLoading(false))
+        }
+    },[season])
+
+    useEffect(()=>{
+      setSportsArray(Array.from(new Set(data?.map((item) => item.Nombre_deporte))));
+    }, [data])
 
   return (
     <div className='max-w-[1350px] w-full'>
         <NavBar />
-        <Sports sports={['Futbol', 'Futbol 7']} onSportChanged={handleSportChanged}/>
+        <SeasonsSelect apiEndPoint={`${API_ENDPOINT}/seasons`} onSeasonChange={(season)=>handleSeasonChange(season)}/>
+        <Sports sports={sportsArray} onSportChanged={(sport:string)=>{handleFilterChange('Nombre_deporte',sport)}} isLoading={isLoading} sportSelected=""/>
         <div className="flex h-[70vh]">
             <aside>
-                <Rankings/>
             </aside>
             <div className="flex-1">
                 <MainContent/>
